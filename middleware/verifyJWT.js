7bb890f239e5747d2e483f41de3489309bc8876f
gfcarlson <39978064+gfcarlson@users.noreply.github.com>
2022-08-22 08:03:11 -0700

    Update 08/22

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+console.log('hello');
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
